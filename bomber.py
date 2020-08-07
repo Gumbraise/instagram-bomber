37d77b31cbd5f5d95a6dc9ea549f17be601925df
lilcsz <43272857+lilcsz@users.noreply.github.com>
2020-08-07 15:07:20 +0300

    Fixed everything!

--- bomber.py
+++ bomber.py
@@ -35,27 +35,15 @@ else:
     print ("Password found: ", password)
     api = InstagramAPI(username, password)
     api.login()
-    istimes = 1
 
-user = input("Enter the victim's IG Username then press ENTER: ")
+user_id = input("Enter the victim's IG User ID: ")
 
-
-url = "https://www.instagram.com/web/search/topsearch/?context=blended&query="+user+"&rank_token=0.3953592318270893&count=1"
-response = requests.get(url)
-respJSON = response.json()
-user_id = str( respJSON['users'][0].get("user").get("pk") )
 
 message = input("Put the message you want the software send and press ENTER: ")
+times = int(input("How many messages do you want to send? "))
 
-if istimes == 0:
-    times = int(input("How many messages do you want to send? "))
-elif istimes == 1:
-    times = NumberOfLine
-
-print("You are gonna STRESS", times,"times", user_id, "with the message: ", message, ".")
-input("Are you sure?")
 
 while times > nostop:
     nostop = nostop + 1
     api.sendMessage(user_id,message)
-    print(nostop, ">> Sent to", user, ": ", message)
+    print(nostop, ">> Sent to", user_id, ": ", message)
