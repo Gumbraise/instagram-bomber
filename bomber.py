dac04b055dcf6231cc08fe03009eb128f73c25de
Kellis <vikiclashonyt@gmail.com>
2020-09-10 14:30:57 +0200

    Made loops to send several different messages

--- bomber.py
+++ bomber.py
@@ -10,42 +10,78 @@
       ░    ░            ░    ░         ░           ░  ░ ░        ░     ░  ░
                                   ░                                        
 """
-from InstagramAPI import InstagramAPI
-import time
-import requests
-import json
-import random
+try:
+    from InstagramAPI import InstagramAPI
+    import time
+    import requests
+    import json
+    import random
+    import sys
 
-nostop = 0
+    while True:
+        try:
+            nostop = 0
 
-accounts = input("Put your Instagram accounts list here (if there is no file just press ENTER): ")
-if not accounts:
-    username = input("Put your IG Username then press ENTER: ")
-    password = input("Put your IG Password then press ENTER: ")
-    api = InstagramAPI(username, password)
-    api.login()
+            while True:
+                accounts = input("Put your Instagram accounts list here (if there is no file just press ENTER): ")
+                if not accounts:
+                    username = input("Put your IG Username then press ENTER: ")
+                    password = input("Put your IG Password then press ENTER: ")
+                    api = InstagramAPI(username, password)
+                    api.login()
+                    break
 
-else:
-    line = random.choice(open(accounts).readlines())
-    username, password = line.split(':')
-    print ("Username found: ", username)
-    print ("Password found: ", password)
-    api = InstagramAPI(username, password)
-    api.login()
+                else:
+                    try:
+                        line = random.choice(open(accounts).readlines())
+                        username, password = line.split(':')
+                        print ("Username found: ", username)
+                        print ("Password found: ", password)
+                        api = InstagramAPI(username, password)
+                        api.login()
+                        break
 
-user = input("Enter the victim's IG Username: ")
-response = requests.get("https://www.instagram.com/"+user+"/?__a=1")
-respJSON = response.json()
-user_id = str( respJSON['graphql'].get("user").get("id") )
+                    except:
+                        print ("Wrong file")
 
+            while True:
+                try:
+                    user = input("Enter the victim's IG Username: ")
+                    try:
+                        response = requests.get("https://www.instagram.com/"+user+"/?__a=1")
+                        respJSON = response.json()
+                        user_id = str( respJSON['graphql'].get("user").get("id") )
+                    except:
+                        print ("Unknown victim's username")
 
-message = input("Put the message you want the software send and press ENTER: ")
-times = int(input("How many messages do you want to send? "))
-proxylist = input("Proxy list (TXT): (If you don't have proxy list press ENTER): ")
-if 'txt' in proxylist:
-    proxy = random.choice(open(proxylist).readlines())
-    api.setProxy(proxy)
-while times > nostop:
-    nostop = nostop + 1
-    api.sendMessage(user_id,message)
-    print(nostop, ">> Sent to", user, ": ", message)
+                    while True:
+                        try:
+                            nostop = 0
+                            message = input("Put the message you want the software send and press ENTER: ")
+
+                            while True:
+                                try:
+                                    times = int(input("How many messages do you want to send? "))
+                                    break
+                                except:
+                                    print ("Wrong number")
+                            
+                            proxylist = input("Proxy list (TXT): (If you don't have proxy list press ENTER): ")
+                            if 'txt' in proxylist:
+                                proxy = random.choice(open(proxylist).readlines())
+                                api.setProxy(proxy)
+                            while times > nostop:
+                                nostop = nostop + 1
+                                api.sendMessage(user_id,message)
+                                print(nostop, ">> Sent to", user, ": ", message)
+                        except KeyboardInterrupt:
+                            print ('\n')
+                            break
+                except KeyboardInterrupt:
+                    print ('\n')
+                    break
+        except KeyboardInterrupt:
+            print ('\nSee you soon :)')
+            break
+except:
+    sys.exit('\nA critical error happened. Please make sure that you executed all commands properly and relaunch the program.')
