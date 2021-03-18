05d01d1f5327e4806bde43734dabd561df8dfdea
Gumbraise <vikiclashonyt@gmail.com>
2021-03-18 19:03:06 +0100

    Added new system

--- bomber.py
+++ bomber.py
@@ -26,11 +26,11 @@ try:
                 accounts = input("Put your Instagram accounts list here (if there is no file just press ENTER): ")
                 if not accounts:
                     username = input("Put your IG Username then press ENTER: ")
-                    try: 
-                        password = getpass.getpass(prompt = "Put your IG Password then press ENTER: ") 
-                    except Exception as error: 
-                        print("Error:", error) 
-                    else: 
+                    try:
+                        password = getpass.getpass(prompt="Put your IG Password then press ENTER: ")
+                    except Exception as error:
+                        print("Error:", error)
+                    else:
                         print("Got Password. Attempting to login.")
                     api = InstagramAPI(username, password)
                     api.login()
@@ -40,26 +40,44 @@ try:
                     try:
                         line = random.choice(open(accounts).readlines())
                         username, password = line.split(':')
-                        print ("Username found: ", username)
-                        print ("Password found: ", password)
+                        print("Username found: ", username)
+                        print("Password found: ", password)
                         api = InstagramAPI(username, password)
                         api.login()
                         break
 
                     except:
-                        print ("Wrong file")
+                        print("Wrong file")
 
             while True:
+                print("Would you prefer enter the user ID or the username (bêta)")
                 try:
-                    user = input("Enter the victim's IG Username: ")
-                    try:
-                        response = requests.get("https://www.instagram.com/"+user+"/?__a=1")
-                        respJSON = response.json()
-                        user_id = str( respJSON['graphql'].get("user").get("id") )
-                    except:
-                        print ("Unknown victim's username")
-                        print ("Program will now terminate. Kindly identify victim account.")
-                        exit(0)
+                    if input("UserID = i, Username = o: ") == "o":
+                        try:
+                            user = input("Enter the victim's IG Username: ")
+                            try:
+                                response = requests.get("https://www.instagram.com/" + user + "/?__a=1")
+                                respJSON = response.json()
+                                user_id = str(respJSON['graphql'].get("user").get("id"))
+                            except:
+                                print("Unknown victim's username")
+                                print("Program will now terminate. Kindly identify victim account.")
+                                exit(0)
+                        except KeyboardInterrupt:
+                            print('\n')
+                            break
+                    else:
+                        while True:
+                             user_id = input("Enter the victim's IG UserID (or press i to get more info about UserID): ")
+
+                             if isinstance(int(user_id), int) == True:
+                                 break;
+                             elif user_id == 'i':
+                                 print("""To found a IG UserID you have to search https://www.instagram.com/{USERNAME}/?__a=1' and to go to graphql=>user=>id.
+The correct id is named id, not fbid.""")
+                                 input("Press enter to continue")
+                             else:
+                                print("This IG UserID is unknown.")
 
                     while True:
                         try:
@@ -70,7 +88,7 @@ try:
                                     times = int(input("How many messages do you want to send? "))
                                     break
                                 except:
-                                    print ("Wrong number")
+                                    print("Wrong number")
 
                             proxylist = input("Proxy list (TXT): (If you don't have proxy list press ENTER): ")
                             if accounts:
@@ -78,16 +96,18 @@ try:
                                 api.setProxy(proxy)
                             while times > nostop:
                                 nostop = nostop + 1
-                                api.sendMessage(user_id,message)
+                                api.sendMessage(user_id, message)
                                 print(nostop, ">> Sent to", user, ": ", message)
                         except KeyboardInterrupt:
-                            print ('\n')
+                            print('\n')
                             break
+
                 except KeyboardInterrupt:
-                    print ('\n')
+                    print('\n')
                     break
         except KeyboardInterrupt:
-            print ('\nSee you soon :)')
+            print('\nSee you soon :)')
             break
 except:
-    sys.exit('\nA critical error happened. Please make sure that you executed all commands properly and relaunch the program.')
+    sys.exit(
+        '\nA critical error happened. Please make sure that you executed all commands properly and relaunch the program.')
